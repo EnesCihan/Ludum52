fc3f9f796e7503426b7c85b31707a5bbc84a8533
engin  can <altinozengincan@gmail.com>
2023-01-09 12:16:49 +0300

    Healing

--- Assets/Scripts/Enemy/Collectable.cs
+++ Assets/Scripts/Enemy/Collectable.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
+    PlayerController playerController;
+    private void Awake()
+    {
+        playerController = FindObjectOfType<PlayerController>();
+    }
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            playerController.currentHealth += Random.Range(10, 16);
             GameManager.SeedCount++;
             Destroy(gameObject);
         }
